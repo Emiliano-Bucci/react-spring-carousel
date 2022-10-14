03f371af22a8d058a6e981814f546f8e324b5128
Emiliano-Bucci <emiliano.bucci@outlook.com>
2022-10-14 16:25:53 +0200

    fix: fixed context typing generic

--- src/useSpringCarousel.tsx
+++ src/useSpringCarousel.tsx
@@ -903,15 +903,15 @@ function useSpringCarousel({
   return { ...res, carouselFragment, thumbsFragment: _thumbsFragment }
 }
 
-type ContextProps<T extends 'use-spring' | 'use-transition'> = Omit<
-  ReturnType<T extends 'use-spring' ? true : false>,
+type ContextProps<T extends 'free-scroll' | undefined> = Omit<
+  ReturnType<T extends 'free-scroll' ? true : false>,
   'carouselFragment' | 'thumbsFragment'
 >
 
 const Context =
-  createContext<ContextProps<'use-spring' | 'use-transition'> | undefined>(undefined)
+  createContext<ContextProps<'free-scroll' | undefined> | undefined>(undefined)
 
-function useSpringCarouselContext<T extends 'use-spring' | 'use-transition'>() {
+function useSpringCarouselContext<T extends 'free-scroll'>() {
   const context = useContext(Context)
   if (!context) {
     throw new Error('useSpringCarouselContext must be used within the carousel.')
