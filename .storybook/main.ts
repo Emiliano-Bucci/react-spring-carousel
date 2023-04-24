888ba19c88c5136255b96c0df48c63164207217d
Emiliano-Bucci <emiliano.bucci@outlook.com>
2023-04-24 10:39:38 +0200

    feat: add light theme to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
