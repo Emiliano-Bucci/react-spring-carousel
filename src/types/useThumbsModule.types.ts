560b52f029366ffe98aee355316c55823d91988d
Emiliano-Bucci <emiliano.bucci@outlook.com>
2023-07-19 16:58:05 +0200

    fix: stuff

--- src/types/useThumbsModule.types.ts
+++ src/types/useThumbsModule.types.ts
@@ -1,13 +1,8 @@
-import {
-  ItemWithThumb,
-  PrepareThumbsData,
-  RenderItemProps,
-  SpringCarouselWithThumbs,
-} from '../types'
+import { ItemWithThumb, PrepareThumbsData, RenderItemProps } from '../types'
 
 export type UseThumbsModule<T extends 'use-spring' | 'use-transition'> = {
   withThumbs?: boolean
-  thumbsSlideAxis: SpringCarouselWithThumbs['thumbsSlideAxis']
+  thumbsSlideAxis: 'x' | 'y'
   prepareThumbsData?: PrepareThumbsData<T>
   items: ItemWithThumb<T>[]
   renderThumbFnProps: RenderItemProps<T>
